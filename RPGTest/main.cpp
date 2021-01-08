b830dc7051a8dab5966deea6132272162b47bf7c
jcmarso <jcmarso2@gmail.com>
2021-01-08 04:32:15 -0500

    FPS Counter added and redundant code removed

--- RPGTest/main.cpp
+++ RPGTest/main.cpp
@@ -1,9 +1,10 @@
 #include <Windows.h>
 #include <string>
 #include <fstream>
+#include <chrono>
 
-#define SCREEN_WIDTH 640			
-#define SCREEN_HEIGHT 448
+#define SCREEN_WIDTH 320			
+#define SCREEN_HEIGHT 224
 #define TILE_WIDTH 32
 #define TILE_HEIGHT 32
 #define TILE_MAP_WIDTH 320
@@ -29,7 +30,7 @@ AssetID charPositition = {"startPoint", 0, 0};	// Initial character position
 // Full map. Composed of tiles of 32x32 dimension. In this case represented by a code formed by A to J for columns,
 // and 1 to 13 for rows, like "A1", "J2" and so on.
 
-string tileMap[(SCREEN_WIDTH / TILE_WIDTH) * (SCREEN_HEIGHT / TILE_HEIGHT)] = {	
+string tileMap[280/*(SCREEN_WIDTH / TILE_WIDTH) * (SCREEN_HEIGHT / TILE_HEIGHT)*/] = {	
 	"I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3", "I3", "J3",
 	"I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4", "I4", "J4",
 	"H2", "A9", "G3", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "A9", "J1", "J1",
@@ -50,45 +51,45 @@ AssetID tiles[(TILE_MAP_WIDTH / TILE_WIDTH) * (TILE_MAP_HEIGHT / TILE_HEIGHT)] =
 AssetID characterSprites[(SPRITE_SHEET_WIDTH / SPRITE_WIDTH) * (SPRITE_SHEET_HEIGHT / SPRITE_HEIGHT)] = {}; // Total number of sprites of characterSpriteSheet
 bool bufferTag = true; // Boolean to keep track or what buffer is being displayed. true = buffer1, false = buffer2
 
-void LoadMapSprite(int x1, int y1, string spriteName);	// Function to load map tiles to screen character array
+void LoadMapTile(int x1, int y1, string tileName);	// Function to load map tiles to screen character array
 void LoadCharacterSprite(int x1, int y1, string spriteName);	// Function to load character sprites to screen character array
 void Update();
 void Input();
 void Draw(bool bufferTag);
 
 wchar_t* screen = new wchar_t[SCREEN_WIDTH * SCREEN_HEIGHT]; // Pointer to character array to be written to console.
-HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 HANDLE buffer1 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 HANDLE buffer2 = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 DWORD dwScreenBufferData = 0;
-wstring appName;
 int prevSprite = 0;
 char keyInput = ' ';
+char prevKeyInput = ' ';
 
 int main() {
-	wstring title(L"One Bit Game Engine");
-	SetConsoleTitle(title.c_str());
-	SetConsoleActiveScreenBuffer(hConsole);
+	auto time1 = std::chrono::system_clock::now();
+	auto time2 = std::chrono::system_clock::now();
+	wstring appName = L"One Bit Game Engine";
+	wchar_t s[256];
+	SetConsoleActiveScreenBuffer(buffer1);
 
 	_COORD coord = { SCREEN_WIDTH, SCREEN_HEIGHT };
 
-	SetConsoleScreenBufferSize(hConsole, coord);
+	SetConsoleScreenBufferSize(buffer1, coord);
 	SetConsoleScreenBufferSize(buffer1, coord);
 	SetConsoleScreenBufferSize(buffer2, coord);
 
 	_CONSOLE_FONT_INFOEX font = { 0 };
 	font.cbSize = sizeof(CONSOLE_FONT_INFOEX);
 	font.nFont;
-	font.dwFontSize.X = 2;
-	font.dwFontSize.Y = 2;
+	font.dwFontSize.X = 4;
+	font.dwFontSize.Y = 4;
 	font.FaceName;
 	font.FontFamily = FF_DONTCARE;
 	font.FontWeight = FW_NORMAL;
 
-	SetCurrentConsoleFontEx(hConsole, false, &font);
 	SetCurrentConsoleFontEx(buffer1, false, &font);
 	SetCurrentConsoleFontEx(buffer2, false, &font);
-	SetConsoleActiveScreenBuffer(hConsole);
+	SetConsoleActiveScreenBuffer(buffer1);
 
 	// Assign codes to tiles
 
@@ -113,6 +114,7 @@ int main() {
 	//Assign codes to character sprites
 	// Reset values
 	letter = 'A';
+
 	number = 1;
 	x = 0;
 	y = 0;
@@ -160,18 +162,27 @@ int main() {
 		}
 	}
 	Update();
-	WriteConsoleOutputCharacter(hConsole, screen, SCREEN_WIDTH * SCREEN_HEIGHT, { 0,0 }, &dwScreenBufferData);
+	WriteConsoleOutputCharacter(buffer1, screen, SCREEN_WIDTH * SCREEN_HEIGHT, { 0,0 }, &dwScreenBufferData);
 
 	// Game loop
 	while (true) { 
 		Input();
 		Draw(bufferTag);
+
+		time2 = std::chrono::system_clock::now();
+		std::chrono::duration<float> passedTime = time2 - time1;
+		time1 = time2;
+		float elapsedTime = passedTime.count();
+
+		swprintf_s(s, 256, L"%s - FPS: %3.2f", appName.c_str(), 1.0f / elapsedTime);
+		SetConsoleTitle(s);
+
 	}
 	return 0;
 }
 
-void LoadMapSprite(int x1, int y1, string spriteName) {
-	AssetID temp = {spriteName, 0, 0};
+void LoadMapTile(int x1, int y1, string tileName) {
+	AssetID temp = {tileName, 0, 0};
 
 	for (int i = 0; i < (SCREEN_WIDTH / TILE_WIDTH) * (SCREEN_HEIGHT / TILE_HEIGHT); i++) {
 		if (tiles[i].name == temp.name) {
@@ -211,7 +222,7 @@ void LoadCharacterSprite(int x1, int y1, string spriteName) {
 void Update() {
 	for (int i = 0; i < SCREEN_WIDTH / TILE_WIDTH; i++) {
 		for (int j = 0; j < SCREEN_HEIGHT / TILE_HEIGHT; j++) {
-			LoadMapSprite(i * TILE_WIDTH, j * TILE_HEIGHT, tileMap[j * (SCREEN_WIDTH / TILE_WIDTH) + i]);
+			LoadMapTile(i * TILE_WIDTH, j * TILE_HEIGHT, tileMap[j * (SCREEN_WIDTH / TILE_WIDTH) + i]);
 		}
 	}
 }
@@ -222,17 +233,18 @@ void Input() {
 		keyInput =  'S';
 
 		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, "A1");
+			LoadCharacterSprite(charPositition.x, charPositition.y += 2, "A1");
 
 		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, "B1");
+			LoadCharacterSprite(charPositition.x, charPositition.y += 2, "B1");
 
 		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, "C1");
+			LoadCharacterSprite(charPositition.x, charPositition.y += 2, "C1");
 
 		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, "B1");
+			LoadCharacterSprite(charPositition.x, charPositition.y += 2, "B1");
 
+		prevKeyInput = keyInput;
 		if (prevSprite < 3)
 			prevSprite += 1;
 		else
@@ -241,17 +253,18 @@ void Input() {
 	else if (GetAsyncKeyState(0x41)) { // A
 		keyInput = 'A';
 		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, "A2");
+			LoadCharacterSprite(charPositition.x -= 2, charPositition.y, "A2");
 
 		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, "B2");
+			LoadCharacterSprite(charPositition.x -= 2, charPositition.y, "B2");
 
 		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, "C2");
+			LoadCharacterSprite(charPositition.x -= 2, charPositition.y, "C2");
 
 		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, "B2");
+			LoadCharacterSprite(charPositition.x -= 2, charPositition.y, "B2");
 
+		prevKeyInput = keyInput;
 		if (prevSprite < 3)
 			prevSprite += 1;
 		else
@@ -261,17 +274,18 @@ void Input() {
 		keyInput = 'D';
 
 		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, "A3");
+			LoadCharacterSprite(charPositition.x += 2, charPositition.y, "A3");
 
 		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, "B3");
+			LoadCharacterSprite(charPositition.x += 2, charPositition.y, "B3");
 
 		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, "C3");
+			LoadCharacterSprite(charPositition.x += 2, charPositition.y, "C3");
 
 		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, "B3");
+			LoadCharacterSprite(charPositition.x += 2, charPositition.y, "B3");
 
+		prevKeyInput = keyInput;
 		if (prevSprite < 3)
 			prevSprite += 1;
 		else
@@ -282,30 +296,31 @@ void Input() {
 		keyInput = 'W';
 
 		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, "A4");
+			LoadCharacterSprite(charPositition.x, charPositition.y -= 2, "A4");
 
 		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, "B4");
+			LoadCharacterSprite(charPositition.x, charPositition.y -= 2, "B4");
 
 		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, "C4");
+			LoadCharacterSprite(charPositition.x, charPositition.y -= 2, "C4");
 
 		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, "B4");
+			LoadCharacterSprite(charPositition.x, charPositition.y -= 2, "B4");
 
+		prevKeyInput = keyInput;
 		if (prevSprite < 3)
 			prevSprite += 1;
 		else
 			prevSprite = 0;
 	}
 	else if(!GetAsyncKeyState(0x53) || !GetAsyncKeyState(0x41) || !GetAsyncKeyState(0x44) || !GetAsyncKeyState(0x57)) {
-		if(keyInput == 'S')
+		if(prevKeyInput == 'S')
 			LoadCharacterSprite(charPositition.x, charPositition.y += 0, "B1");
-		else if (keyInput == 'A')
+		else if (prevKeyInput == 'A')
 			LoadCharacterSprite(charPositition.x, charPositition.y += 0, "B2");
-		else if (keyInput == 'D')
+		else if (prevKeyInput == 'D')
 			LoadCharacterSprite(charPositition.x, charPositition.y += 0, "B3");
-		else if (keyInput == 'W')
+		else if (prevKeyInput == 'W')
 			LoadCharacterSprite(charPositition.x, charPositition.y += 0, "B4");
 	}
 }
