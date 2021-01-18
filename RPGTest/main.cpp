166eb63c30b525ee29d8793ee7269f2c74cb35de
jcmarso <jcmarso2@gmail.com>
2021-01-17 19:08:40 -0500

    Player sprites resized and screen border collision implemented

--- RPGTest/main.cpp
+++ RPGTest/main.cpp
@@ -12,11 +12,11 @@
 #define TILE_MAP_WIDTH 320
 #define TILE_MAP_HEIGHT 416
 #define SPRITE_WIDTH 32
-#define SPRITE_HEIGHT 48
+#define SPRITE_HEIGHT 32
 #define SPRITE_SHEET_WIDTH 96
 #define SPRITE_SHEET_HEIGHT 192
-#define MAP_WIDTH 1104
-#define MAP_HEIGHT 648
+#define MAP_WIDTH 1472
+#define MAP_HEIGHT 864
 
 using namespace std;
 
@@ -25,20 +25,14 @@ struct AssetID	// Used for map tiles and character sprites IDs
 	int id = 0, x = 0, y = 0;
 };
 
-struct Camera	// Camera Initial position
-{
-	int x = 0, y = 0;
-};
 
 wstring tileSheet;	// To store tile sheet
 wstring characterSpriteSheet;	// To store character sprite sheet
-//wstring fullMap; // To store full maps
-AssetID charPositition = {0, 288, 64};	// Initial character position
-//Camera camera = { 0, 0 };
-
-// Full map. Composed of tiles of 32x32 dimension.
-
-vector<int> map; // Map composed of 46x27 tiles of 32x32 pixels
+vector<int> map; // To store full map. Composed of 46x27 tiles of 32x32 pixels
+int playerPositionX = 32;
+int playerPositionY = 48;
+int cameraPositionX = 0;
+int cameraPositionY = 0;
 
 AssetID tiles[(TILE_MAP_WIDTH / TILE_WIDTH) * (TILE_MAP_HEIGHT / TILE_HEIGHT)] = {}; // Total number of tiles on tileSheet
 AssetID characterSprites[(SPRITE_SHEET_WIDTH / SPRITE_WIDTH) * (SPRITE_SHEET_HEIGHT / SPRITE_HEIGHT)] = {}; // Total number of sprites of characterSpriteSheet
@@ -133,7 +127,7 @@ int main() {
 	}
 
 	// Load character sprite sheet to characterSpriteSheet variable
-	ifstream file2("character.dat");
+	ifstream file2("player32x32.dat");
 	if (file2.is_open())
 	{
 		while (getline(file2, temp))
@@ -224,103 +218,114 @@ void Update() {
 }
 
 void Input() {
-	Update();
+	
 	
 	if (GetAsyncKeyState(0x53)) {	// S
 		keyInput =  'S';
 		//camera.y += 4;
-		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, 1);
+		if (playerPositionY < SCREEN_HEIGHT - SPRITE_HEIGHT) {
+			Update();
+			if (prevSprite == 0)
+				LoadCharacterSprite(playerPositionX, playerPositionY += 4, 1);
 
-		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, 2);
+			else if (prevSprite == 1)
+				LoadCharacterSprite(playerPositionX, playerPositionY += 4, 2);
 
-		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, 3);
+			else if (prevSprite == 2)
+				LoadCharacterSprite(playerPositionX, playerPositionY += 4, 3);
 
-		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x, charPositition.y += 4, 2);
+			else if (prevSprite == 3)
+				LoadCharacterSprite(playerPositionX, playerPositionY += 4, 2);
+		}
+			prevKeyInput = keyInput;
+			if (prevSprite < 3)
+				prevSprite += 1;
+			else
+				prevSprite = 0;
 
-		prevKeyInput = keyInput;
-		if (prevSprite < 3)
-			prevSprite += 1;
-		else
-			prevSprite = 0;
 	}
 	else if (GetAsyncKeyState(0x41)) { // A
 		keyInput = 'A';
 		//camera.x -= 4;
-		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, 4);
+		if (playerPositionX > 0) {
+			Update();
+			if (prevSprite == 0)
+				LoadCharacterSprite(playerPositionX -= 4, playerPositionY, 4);
 
-		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, 5);
+			else if (prevSprite == 1)
+				LoadCharacterSprite(playerPositionX -= 4, playerPositionY, 5);
 
-		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, 6);
+			else if (prevSprite == 2)
+				LoadCharacterSprite(playerPositionX -= 4, playerPositionY, 6);
 
-		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x -= 4, charPositition.y, 5);
-
-		prevKeyInput = keyInput;
-		if (prevSprite < 3)
-			prevSprite += 1;
-		else
-			prevSprite = 0;
+			else if (prevSprite == 3)
+				LoadCharacterSprite(playerPositionX -= 4, playerPositionY, 5);
+		}
+			prevKeyInput = keyInput;
+			if (prevSprite < 3)
+				prevSprite += 1;
+			else
+				prevSprite = 0;
 	}
 	else if (GetAsyncKeyState(0x44)) {	// D
 		keyInput = 'D';
 		//camera.x += 4;
-		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, 7);
+		if (playerPositionX < SCREEN_WIDTH - SPRITE_WIDTH) {
+			Update();
+			if (prevSprite == 0)
+				LoadCharacterSprite(playerPositionX += 4, playerPositionY, 7);
 
-		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, 8);
+			else if (prevSprite == 1)
+				LoadCharacterSprite(playerPositionX += 4, playerPositionY, 8);
 
-		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, 9);
+			else if (prevSprite == 2)
+				LoadCharacterSprite(playerPositionX += 4, playerPositionY, 9);
 
-		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x += 4, charPositition.y, 8);
-
-		prevKeyInput = keyInput;
-		if (prevSprite < 3)
-			prevSprite += 1;
-		else
-			prevSprite = 0;
+			else if (prevSprite == 3)
+				LoadCharacterSprite(playerPositionX += 4, playerPositionY, 8);
+		}
+			prevKeyInput = keyInput;
+			if (prevSprite < 3)
+				prevSprite += 1;
+			else
+				prevSprite = 0;
 
 	}
 	else if (GetAsyncKeyState(0x57)) { // W
 		keyInput = 'W';
 		//camera.y -= 4;
-		if (prevSprite == 0)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, 10);
+		if (playerPositionY > 0) {
+			Update();
+			if (prevSprite == 0)
+				LoadCharacterSprite(playerPositionX, playerPositionY -= 4, 10);
 
-		else if (prevSprite == 1)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, 11);
+			else if (prevSprite == 1)
+				LoadCharacterSprite(playerPositionX, playerPositionY -= 4, 11);
 
-		else if (prevSprite == 2)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, 12);
+			else if (prevSprite == 2)
+				LoadCharacterSprite(playerPositionX, playerPositionY -= 4, 12);
 
-		else if (prevSprite == 3)
-			LoadCharacterSprite(charPositition.x, charPositition.y -= 4, 11);
-
-		prevKeyInput = keyInput;
-		if (prevSprite < 3)
-			prevSprite += 1;
-		else
-			prevSprite = 0;
+			else if (prevSprite == 3)
+				LoadCharacterSprite(playerPositionX, playerPositionY -= 4, 11);
+		}
+			prevKeyInput = keyInput;
+			if (prevSprite < 3)
+				prevSprite += 1;
+			else
+				prevSprite = 0;
 	}
 	else if(!GetAsyncKeyState(0x53) || !GetAsyncKeyState(0x41) || !GetAsyncKeyState(0x44) || !GetAsyncKeyState(0x57)) {
+		Update();
 		if(prevKeyInput == 'S')
-			LoadCharacterSprite(charPositition.x, charPositition.y += 0, 2);
+			LoadCharacterSprite(playerPositionX, playerPositionY += 0, 2);
 		else if (prevKeyInput == 'A')
-			LoadCharacterSprite(charPositition.x, charPositition.y += 0, 5);
+			LoadCharacterSprite(playerPositionX, playerPositionY += 0, 5);
 		else if (prevKeyInput == 'D')
-			LoadCharacterSprite(charPositition.x, charPositition.y += 0, 8);
+			LoadCharacterSprite(playerPositionX, playerPositionY += 0, 8);
 		else if (prevKeyInput == 'W')
-			LoadCharacterSprite(charPositition.x, charPositition.y += 0, 11);
+			LoadCharacterSprite(playerPositionX, playerPositionY += 0, 11);
 	}
+	
 }
 void Draw(bool bufferTag) {
 	HANDLE tempBuffer;
@@ -344,5 +349,5 @@ void Draw(bool bufferTag) {
 		WriteConsoleOutputCharacter(tempBuffer, screen, SCREEN_WIDTH * SCREEN_HEIGHT, { 0,0 }, &dwScreenBufferData);
 		bufferTag = false;
 	}
-	//Sleep(25); // To cap FPS to 20 approximately and keep animation at normal speed.
+	Sleep(25); // To cap FPS to 20 approximately and keep animation at normal speed.
 }
